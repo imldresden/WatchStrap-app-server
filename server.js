219bd425d36cf30265a3d3cd5694d3ca94eeb52b
Tom Horak <dev@tom-horak.de>
2019-08-08 10:45:27 +0200

    Added convert method on server side.
    Converst base64 image into bytearray for the eink display.
    Added multiple packages.

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
 const ip = require('ip');
+const einkConverter = require('image_to_epaper_converter');
+const imagejs = require('imagejs');
+const { createCanvas, loadImage } = require('canvas')
+const Dither = require('canvas-dither');
 
 // Configuration
 const idMain = "MAIN";
@@ -29,14 +33,14 @@ const connectionMap = new Map();
 
 io.on('connection', client => {
     client.on('handshake', identifier => {
-        connectionMap[identifier] = client;
+        connectionMap.set(identifier, client);
         if (remoteAddress) {
             client.emit('redirect', remoteAddress);
         } else {
             client.emit('handshake', 'ok');
         }
-        if (identifier !== idMain && connectionMap[idMain] !== undefined) {
-            connectionMap[idMain].emit('info', {
+        if (identifier !== idMain && connectionMap.has(idMain)) {
+            connectionMap.get(idMain).emit('info', {
                 type: 'connect',
                 identifier: identifier
             });
@@ -44,12 +48,101 @@ io.on('connection', client => {
     });
 
     client.on('msg', msg => {
-        let target = connectionMap[msg.target];
+        let target = connectionMap.get(msg.target);
         if (target)
             target.emit('msg', msg);
         /*else
             client.emit('err', {'error': 'TargetNotFound'});*/
     });
+
+    client.on('convert', msg => {
+        // Parse base64 string to a bitmap
+        loadImage(msg.payload)
+        .then((img) => {
+            // img = Image object from the canvas library
+            // Create canvas to generate a bitmap
+            let canvas = createCanvas(msg.size.width, msg.size.height)
+            let ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            // Apply dithering if requested
+            if (msg.dithering) {
+                let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                imgData = Dither.atkinson(imgData);
+                ctx.putImageData(imgData, 0, 0);
+            }
+            if (msg.invert) {
+                let imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                let data = imgData.data;
+                for (let i=0; i < data.length; i+=4) {
+                    data[i] = 255 - data[i];
+                    data[i+1] = 255 - data[i+1];
+                    data[i+2] = 255 - data[i+2];
+                }
+                ctx.putImageData(imgData, 0, 0);
+            }
+            // Generate bitmap via JPEGStream
+            stream = canvas.createJPEGStream({
+                quality: 1,
+                progressive: false,
+                chromaSubsampling: true});
+            let bitmap = new imagejs.Bitmap();
+            bitmap.read(stream, { type: imagejs.ImageType.JPG })
+                .then(function() { 
+                    // bitmap.writeFile('image2.jpg', { quality:75 }).then(function() {
+                    //     //console.log("2nd bitmap has been saved");
+                    // });
+
+                    // Convert bitmap to byteArray for eink display
+                    einkConverter.convert({
+                        source_file: '',
+                        tasks: 'hexadecimal',
+                        read_from_file: false,
+                        bitmap: bitmap,
+                        return_array: true,
+                        display: {
+                            width: 104,
+                            height: 212,
+                            fitmode: "none",
+                            colormode: msg.invert ? 'invert' : 'normal',
+                            fillmode: msg.invert ? 'invert' : 'normal'
+                        }
+                    }).then(byteArray => {
+                        let dataAsInt = byteArray.map(byte => parseInt(byte, 16));
+            
+                        // Split array into three parts to avoid memory issues on micro controller
+                        let msg1 = {
+                            target: msg.target,
+                            type: msg.type,
+                            part: 0,
+                            payload: dataAsInt.slice(0, Math.floor(byteArray.length / 3))
+                        };
+                        let msg2 = {
+                            target: msg.target,
+                            type: msg.type,
+                            part: 1,
+                            payload: dataAsInt.slice(Math.floor(byteArray.length / 3), Math.floor(byteArray.length * (2/3)))
+                        };
+                        let msg3 = {
+                            target: msg.target,
+                            type: msg.type,
+                            part: 2,
+                            payload: dataAsInt.slice(Math.floor(byteArray.length * (2/3)), dataAsInt.length)
+                        };
+                        try {            
+                            connectionMap.get(msg.target).emit('msg', msg1);
+                            connectionMap.get(msg.target).emit('msg', msg2);
+                            connectionMap.get(msg.target).emit('msg', msg3);
+                        }
+                        catch(e) {
+                            // Socket for loStrap not available
+                        }
+                    })
+                    .catch(error => console.log(error));
+                });
+        }).catch(err => {
+            // Error converting image
+          });        
+    })
 
     client.on('disconnect', (reason) => {
         let identifier = getIdentifierByClientId(client.id);
